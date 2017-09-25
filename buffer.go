808b3b622c5036392d189c2ac7fab0dcdadfcb5e
Michael Woolnough <michael.woolnough@gmail.com>
2017-09-25 14:12:19 +0100

    added comments

--- buffer.go
+++ buffer.go
@@ -1,3 +1,5 @@
+// Package httpbuffer provides a buffer for HTTP requests so that the
+// Content-Length may be set.
 package httpbuffer
 
 import (
@@ -30,12 +32,18 @@ var (
 	}
 )
 
+// Handler wraps a http.Handler and provides a buffer and possible gzip
+// compression. It buffers the Writes and sends the Content-Length header
+// before Writing the buffer to the client.
+//
+// The compress flag, when true, enables gzip compression for the output.
 type Handler struct {
 	http.Handler
 	Compress bool
 }
 
-func (h Handler) ServerHTTP(w http.ResponseWriter, r *http.Request) {
+// ServeHTTP implements the http.Handler interface
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	var compressed bool
 
 	if h.Compress {

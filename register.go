affb499d80ebcbe6f2b061f1971deb1c0e0cc6db
Michael Woolnough <michael.woolnough@gmail.com>
2017-11-23 08:02:55 +0000

    added way to add encoding types

---
+++ register.go
@@ -0,0 +1,40 @@
+package httpbuffer
+
+import (
+	"io"
+)
+
+var encodings = map[string]Encoding{
+	"": identity{},
+}
+
+type identity struct{}
+
+func (identity) Open(w io.Writer) io.Writer {
+	return w
+}
+
+func (identity) Close(io.Writer) {}
+
+func (identity) Name() string {
+	return ""
+}
+
+// Encoding represents a type that applies a Coding to a byte stream
+type Encoding interface {
+	// Open takes a buffer and returns an encoder-wrapped buffer
+	Open(io.Writer) io.Writer
+
+	// Close returns the encoder-wrapped buffer to flush/close and release
+	// resources
+	Close(io.Writer)
+
+	// Name returns the identifier for the encoding algorithm
+	Name() string
+}
+
+// Register registers the encoding for the buffers to use. Should not be used
+// passed initialisation.
+func Register(e Encoding) {
+	encodings[e.Name()] = e
+}
